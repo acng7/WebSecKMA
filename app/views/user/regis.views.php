dbecbe09af9ad2b206a5f1703961d811f4539a6a
anjin <146029519+acng7@users.noreply.github.com>
2025-10-31 12:21:26 +0700

    Update regis.views.php

--- app/views/user/regis.views.php
+++ app/views/user/regis.views.php
@@ -3,51 +3,57 @@
   <div id="title-container4">
     <h1>REGISTER</h1>
   </div>
+  
+  <?php if($error = getFlashMessage('error')): ?>
+    <div class="alert alert-danger"><?= e($error) ?></div>
+  <?php endif; ?>
+  
+  <?php if($success = getFlashMessage('success')): ?>
+    <div class="alert alert-success"><?= e($success) ?></div>
+  <?php endif; ?>
+  
 <form action="" method="post">
+  <input type="hidden" name="csrf_token" value="<?= e(generateCsrfToken()) ?>">
+  
   <div class="form-outline mb-4">
-    <input type="text" id="" class="form-control" maxlength="255" required name="userName" />
-    <label class="form-label" for="">Tên tài khoản</label>
+    <input type="text" id="userName" class="form-control" maxlength="255" required name="userName" autocomplete="username" />
+    <label class="form-label" for="userName">Tên tài khoản</label>
   </div>
 
   <div class="form-outline mb-4">
-    <input type="password" id="" class="form-control" maxlength="255" required name="password" />
-    <label class="form-label" for="">Mật khẩu</label>
+    <input type="password" id="password" class="form-control" maxlength="255" required name="password" autocomplete="new-password" />
+    <label class="form-label" for="password">Mật khẩu</label>
   </div>
   
   <div class="form-outline mb-4">
-    <input type="text" id="" class="form-control" maxlength="255" required name="fullName" />
-    <label class="form-label" for="">Họ và tên</label>
+    <input type="text" id="fullName" class="form-control" maxlength="255" required name="fullName" autocomplete="name" />
+    <label class="form-label" for="fullName">Họ và tên</label>
   </div>
+  
   <div class="form-outline mb-4">
-    <select name="role_id" class="form-control" id="" required>
-      <?php
-      foreach ($getAllRole as $key => $value) {
-        echo'<option value="'.$value['id_role'].'">'.$value['nameRole'].'</option>';
-      }
-      ?>
+    <select name="role_id" class="form-control" id="role_id" required>
+      <option value="">-- Chọn vai trò --</option>
+      <?php foreach ($getAllRole as $role): ?>
+        <option value="<?= e($role['id_role']) ?>"><?= e($role['nameRole']) ?></option>
+      <?php endforeach; ?>
     </select>
-    <label class="form-label" for="">Vai trò bạn muốn đăng ký</label>
+    <label class="form-label" for="role_id">Vai trò bạn muốn đăng ký</label>
   </div>
 
-  <!-- Submit button -->
   <button type="submit" name="btn-regis" class="btn btn-primary btn-block mb-4">Đăng ký</button>
 
-  <!-- Register buttons -->
   <div class="text-center">
     <p>Already have an account? <a href="index.php?type=login">Sign in</a></p>
     <p>or sign up with:</p>
     <button type="button" class="btn btn-link btn-floating mx-1">
       <i class="fab fa-facebook-f"></i>
     </button>
-
     <button type="button" class="btn btn-link btn-floating mx-1">
       <i class="fab fa-google"></i>
     </button>
-
     <button type="button" class="btn btn-link btn-floating mx-1">
       <i class="fab fa-twitter"></i>
     </button>
-
     <button type="button" class="btn btn-link btn-floating mx-1">
       <i class="fab fa-github"></i>
     </button>
