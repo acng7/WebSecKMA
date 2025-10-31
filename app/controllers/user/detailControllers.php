3ade0f6a4e5b1071cd87752736ea93c3b06d7513
anjin <146029519+acng7@users.noreply.github.com>
2025-10-31 12:24:55 +0700

    Update detailControllers.php

--- app/controllers/user/detailControllers.php
+++ app/controllers/user/detailControllers.php
@@ -1,3 +1,15 @@
 <?php
-$getPostById1=getPostById1($_GET['id']);
+// Validate ID
+if(!isset($_GET['id']) || !validateId($_GET['id'])){
+    setFlashMessage('error', 'Bài viết không hợp lệ');
+    safeRedirect('index.php');
+}
+
+$getPostById1 = getPostById1($_GET['id']);
+
+if(!is_array($getPostById1)){
+    setFlashMessage('error', 'Bài viết không tồn tại hoặc đã bị ẩn');
+    safeRedirect('index.php');
+}
+
 include_once("app/views/user/postdetail.views.php");
