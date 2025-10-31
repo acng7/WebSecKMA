be1a01bef65bc101c11f4898948106991c5570bd
anjin <146029519+acng7@users.noreply.github.com>
2025-10-31 12:24:39 +0700

    Update cateControllers.php

--- app/controllers/user/cateControllers.php
+++ app/controllers/user/cateControllers.php
@@ -1,7 +1,16 @@
 <?php
-$getCategoryById=getCategoryById($_GET['id']);
-if(!is_array($getCategoryById)){
-    header("Location: index.php");
+// Validate ID
+if(!isset($_GET['id']) || !validateId($_GET['id'])){
+    setFlashMessage('error', 'Danh mục không hợp lệ');
+    safeRedirect('index.php');
 }
-$getAllPostByIdCate=getAllPostByIdCate($_GET['id']);
+
+$getCategoryById = getCategoryById($_GET['id']);
+
+if(!is_array($getCategoryById)){
+    setFlashMessage('error', 'Danh mục không tồn tại');
+    safeRedirect('index.php');
+}
+
+$getAllPostByIdCate = getAllPostByIdCate($_GET['id']);
 include_once("app/views/user/category.views.php");
