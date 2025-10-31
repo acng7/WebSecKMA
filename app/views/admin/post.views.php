0b63e84df75b2bd7e40de68f1d0b06ccfd546c71
anjin <146029519+acng7@users.noreply.github.com>
2025-10-31 12:13:31 +0700

    Update post.views.php

--- app/views/admin/post.views.php
+++ app/views/admin/post.views.php
@@ -1,5 +1,14 @@
 <h1>QUẢN LÝ BÀI VIẾT</h1>
-<table class="table">
+
+<?php if($error = getFlashMessage('error')): ?>
+  <div class="alert alert-danger"><?= e($error) ?></div>
+<?php endif; ?>
+
+<?php if($success = getFlashMessage('success')): ?>
+  <div class="alert alert-success"><?= e($success) ?></div>
+<?php endif; ?>
+
+<table class="table table-striped">
   <thead>
     <tr>
       <th scope="col">STT</th>
@@ -17,83 +26,50 @@
   <tbody>
     <?php
     $i=1;
-        foreach ($getAllPost1 as $key) {
-            echo'
-            <tr>
-      <th scope="row">'.$i++.'</th>
-      <td><img src="'.$key['postImage'].'" width="50" /></td>
-      <td>'.$key['postName'].'</td>
-      <td>'.$key['postDescription'].'</td>
-      <td>'.$key['postDate'].'</td>
-      <td>'.$key['nameCategory'].'</td>
-      <td>'.$key['fullName'].'</td>';
-      switch ($key['isAccepted']) {
-        case 0:
-            echo '<td>Được duyệt</td>';
-            break;
-            case 1:
-                echo '<td>Đang chờ duyệt đăng</td>';
-            
-                break;
-                case 2:
-                    echo '<td>Đang chờ duyệt sửa</td>';
-            
-                    break;
-                    case 3:
-                        echo '<td>Đang chờ duyệt xóa</td>';
-                        break;
-                        case 4:
-                          echo '<td>Bị ẩn</td>';
-                          break;
+    foreach ($getAllPost1 as $key) {
+        echo '<tr>';
+        echo '<th scope="row">'.e($i++).'</th>';
+        echo '<td><img src="'.e($key['postImage']).'" width="50" alt="Post" /></td>';
+        echo '<td>'.e($key['postName']).'</td>';
+        echo '<td>'.e($key['postDescription']).'</td>';
+        echo '<td>'.e($key['postDate']).'</td>';
+        echo '<td>'.e($key['nameCategory']).'</td>';
+        echo '<td>'.e($key['fullName']).'</td>';
         
-        default:
-            # code...
-            break;
-      }
-      echo'
-      <td><a href="index.php?type=post&type1=seepost&id='.$key['id_post'].'">Xem</a></td>
-      ';
-      switch ($key['isAccepted']) {
-        case 0:
-          echo'
-          <td><a href="index.php?type=post&hide='.$key['id_post'].'">Ẩn</a></td>';
-            break;
-            case 1:
-              echo'
-              <td><a href="index.php?type=post&confirm='.$key['id_post'].'">Duyệt đăng</a></td>';
-            
-                break;
-                case 2:
-                  echo'
-                  <td><a href="index.php?type=post&confirm='.$key['id_post'].'">Duyệt sửa</a></td>';
-            
-                    break;
-                    case 3:
-                      echo'
-                      <td><a href="index.php?type=post&del='.$key['id_post'].'">Duyệt xóa</a></td>';
-                        break;
-                        case 4:
-                          echo'
-                          <td><a href="index.php?type=post&unhide='.$key['id_post'].'">Bỏ ẩn</a></td>';
-                            break;
-        default:
-            break;
-      }
-      // if($key['isAccepted']==0){
-      //   echo'
-      //   <td><a href="index.php?type=postself&del='.$key['id_post'].'">Xóa</a></td>';
-      // }else if($key['isAccepted']==3){
-      //   echo'
-      //   <td><a href="index.php?type=postself&restore='.$key['id_post'].'">Khôi phục</a></td>';
-      // }else{
-      //   echo'
-      //   <td><a>Chưa hoàn tất phê duyệt</a></td>';
-      // }
-      echo'
-    </tr>
-            ';
+        switch ($key['isAccepted']) {
+            case 0: echo '<td><span class="badge bg-success">Được duyệt</span></td>'; break;
+            case 1: echo '<td><span class="badge bg-warning">Chờ duyệt đăng</span></td>'; break;
+            case 2: echo '<td><span class="badge bg-info">Chờ duyệt sửa</span></td>'; break;
+            case 3: echo '<td><span class="badge bg-danger">Chờ duyệt xóa</span></td>'; break;
+            case 4: echo '<td><span class="badge bg-secondary">Bị ẩn</span></td>'; break;
+            default: echo '<td><span class="badge bg-secondary">Không xác định</span></td>'; break;
         }
+        
+        echo '<td><a href="index.php?type=post&type1=seepost&id='.e($key['id_post']).'" class="btn btn-sm btn-info">Xem</a></td>';
+        
+        switch ($key['isAccepted']) {
+            case 0:
+                echo '<td><a href="index.php?type=post&hide='.e($key['id_post']).'" class="btn btn-sm btn-secondary" onclick="return confirm(\'Ẩn bài viết này?\')">Ẩn</a></td>';
+                break;
+            case 1:
+                echo '<td><a href="index.php?type=post&confirm='.e($key['id_post']).'" class="btn btn-sm btn-success" onclick="return confirm(\'Duyệt bài viết này?\')">Duyệt đăng</a></td>';
+                break;
+            case 2:
+                echo '<td><a href="index.php?type=post&confirm='.e($key['id_post']).'" class="btn btn-sm btn-success" onclick="return confirm(\'Duyệt sửa bài viết này?\')">Duyệt sửa</a></td>';
+                break;
+            case 3:
+                echo '<td><a href="index.php?type=post&del='.e($key['id_post']).'" class="btn btn-sm btn-danger" onclick="return confirm(\'Xóa vĩnh viễn bài viết này?\')">Duyệt xóa</a></td>';
+                break;
+            case 4:
+                echo '<td><a href="index.php?type=post&unhide='.e($key['id_post']).'" class="btn btn-sm btn-success" onclick="return confirm(\'Bỏ ẩn bài viết này?\')">Bỏ ẩn</a></td>';
+                break;
+            default:
+                echo '<td>-</td>';
+                break;
+        }
+        
+        echo '</tr>';
+    }
     ?>
-    
   </tbody>
 </table>
