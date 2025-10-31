38300b1edfa5658601113508cb26a97163b73d2f
anjin <146029519+acng7@users.noreply.github.com>
2025-10-31 12:26:11 +0700

    Update userControllers.php

--- app/controllers/admin/userControllers.php
+++ app/controllers/admin/userControllers.php
@@ -1,9 +1,32 @@
 <?php
-        if(isset($_GET['active'])){
-            activeUser($_GET['active']);
+// Authorization check - only Admin can access
+requireAuth([1]);
+
+if(isset($_GET['active'])){
+    if(!validateId($_GET['active'])){
+        setFlashMessage('error', 'ID không hợp lệ');
+    }else{
+        if(activeUser($_GET['active'])){
+            setFlashMessage('success', 'Kích hoạt tài khoản thành công');
+        }else{
+            setFlashMessage('error', 'Kích hoạt tài khoản thất bại');
         }
-        if(isset($_GET['unactive'])){
-            unActiveUser($_GET['unactive']);
+    }
+    safeRedirect('index.php?type=user');
+}
+
+if(isset($_GET['unactive'])){
+    if(!validateId($_GET['unactive'])){
+        setFlashMessage('error', 'ID không hợp lệ');
+    }else{
+        if(unActiveUser($_GET['unactive'])){
+            setFlashMessage('success', 'Vô hiệu hóa tài khoản thành công');
+        }else{
+            setFlashMessage('error', 'Vô hiệu hóa tài khoản thất bại');
         }
-            $getAllUser=getAllUser();
-    include_once("app/views/admin/user.views.php");
+    }
+    safeRedirect('index.php?type=user');
+}
+
+$getAllUser = getAllUser();
+include_once("app/views/admin/user.views.php");
