26114cdc746010739ae561a0bbb00578ac297475
anjin <146029519+acng7@users.noreply.github.com>
2025-10-31 12:12:15 +0700

    Update fixpost.views.php

--- app/views/admin/fixpost.views.php
+++ app/views/admin/fixpost.views.php
@@ -3,24 +3,36 @@
   <div id="title-container4">
     <h1>SỬA BÀI VIẾT</h1>
   </div>
+  
+  <?php if($error = getFlashMessage('error')): ?>
+    <div class="alert alert-danger"><?= e($error) ?></div>
+  <?php endif; ?>
+  
+  <?php if($success = getFlashMessage('success')): ?>
+    <div class="alert alert-success"><?= e($success) ?></div>
+  <?php endif; ?>
+  
 <form action="" method="post">
+  <input type="hidden" name="csrf_token" value="<?= e(generateCsrfToken()) ?>">
+  
   <div class="form-outline mb-4">
-    <img width="20%" src="<?php echo $getPostByIdAndIdAuthor['postImage'];?>" alt="" srcset="">
-    <input type="text" id="" name="postImage" value="<?php echo $getPostByIdAndIdAuthor['postImage'];?>" class="form-control" required />
-    <label for="">Link Ảnh</label>
+    <img width="20%" src="<?= e($getPostByIdAndIdAuthor['postImage']) ?>" alt="Post Image" />
+    <input type="url" id="postImage" name="postImage" value="<?= e($getPostByIdAndIdAuthor['postImage']) ?>" class="form-control" required />
+    <label for="postImage">Link Ảnh</label>
   </div>
   <div class="form-outline mb-4">
-    <input type="text" id="" name="postName" value="<?php echo $getPostByIdAndIdAuthor['postName'];?>" class="form-control" required />
-    <label for="">Tên bài viết</label>
+    <input type="text" id="postName" name="postName" value="<?= e($getPostByIdAndIdAuthor['postName']) ?>" class="form-control" maxlength="255" required />
+    <label for="postName">Tên bài viết</label>
   </div>
   <div class="form-outline mb-4">
-    <input type="text" id="" name="postDescription" value="<?php echo $getPostByIdAndIdAuthor['postDescription'];?>" class="form-control" required />
-    <label for="">Mô tả bài viết</label>
+    <input type="text" id="postDescription" name="postDescription" value="<?= e($getPostByIdAndIdAuthor['postDescription']) ?>" class="form-control" maxlength="255" required />
+    <label for="postDescription">Mô tả bài viết</label>
   </div>
   <div class="form-outline mb-4">
-    <textarea name="postContent" class="form-control" id="" required><?php echo $getPostByIdAndIdAuthor['postContent'];?></textarea>
-    <label for="">Nội dung bài viết</label>
+    <textarea name="postContent" class="form-control" id="postContent" rows="10" required><?= e($getPostByIdAndIdAuthor['postContent']) ?></textarea>
+    <label for="postContent">Nội dung bài viết</label>
   </div>
   <button type="submit" name="btn-save-post" class="btn btn-primary btn-block mb-4">LƯU</button>
+  <a href="index.php?type=postself" class="btn btn-secondary btn-block mb-4">Quay lại</a>
 </form>
 </div>
