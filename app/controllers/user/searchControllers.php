ef4c9def0edb29da3353e84faa59d4b11791ff91
anjin <146029519+acng7@users.noreply.github.com>
2025-10-31 12:24:23 +0700

    Update searchControllers.php

--- app/controllers/user/searchControllers.php
+++ app/controllers/user/searchControllers.php
@@ -1,9 +1,24 @@
 <?php
 if(isset($_POST['search']) && isset($_POST['btn-search'])){
-    $search=$_POST['search'];
-    $getAllPostBySearch=getAllPostBySearch($search);
+    // Verify CSRF token
+    if(!isset($_POST['csrf_token']) || !verifyCsrfToken($_POST['csrf_token'])){
+        $getAllPostBySearch = [];
+        $search = '';
+    }else{
+        $search = sanitizeInput($_POST['search']);
+        
+        if(empty($search)){
+            $getAllPostBySearch = [];
+        }elseif(strlen($search) > 255){
+            $search = substr($search, 0, 255);
+            $getAllPostBySearch = getAllPostBySearch($search);
+        }else{
+            $getAllPostBySearch = getAllPostBySearch($search);
+        }
+    }
 }else{
-    $getAllPostBySearch=[];
-    $search='';
+    $getAllPostBySearch = [];
+    $search = '';
 }
+
 include_once("app/views/user/search.views.php");
