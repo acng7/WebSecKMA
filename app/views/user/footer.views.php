36f5c38b1e54809c8c8cfa04ccee1f70adf22baa
anjin <146029519+acng7@users.noreply.github.com>
2025-11-14 15:41:29 +0700

    Update footer.views.php

--- app/views/user/footer.views.php
+++ app/views/user/footer.views.php
@@ -18,43 +18,43 @@
       <div class="container text-center text-md-start mt-5">
         <div class="row mt-3">
           <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold">Trangcosmestics</h6>
+            <h6 class="text-uppercase fw-bold">Nhóm 1</h6>
             <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-            <p>Nhiệm vụ của Trangcosmestics là tăng sắc đẹp cho mọi người thông qua từng sản phẩm chất lượng nhất</p>
+            <p>Nhiệm vụ của Nhóm 1 là tăng cường bảo mật, đảm bảo hệ thống khách hàng sẽ luôn hoạt động tốt.</p>
           </div>
 
           <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
             <h6 class="text-uppercase fw-bold">SẢN PHẨM</h6>
             <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-            <p><a href="#" class="text-white">Chăm sóc da</a></p>
-            <p><a href="#" class="text-white">Trang điểm</a></p>
-            <p><a href="#" class="text-white">Chăm sóc cơ thể</a></p>
-            <p><a href="#" class="text-white">Chăm sóc móng</a></p>
+            <p><a href="#" class="text-white">Pentest</a></p>
+            <p><a href="#" class="text-white">Giám sát hệ thống</a></p>
+            <p><a href="#" class="text-white">Ngăn chặn, xử lý sự cố</a></p>
+            <p><a href="#" class="text-white">Tư vấn bảo mật an toàn</a></p>
           </div>
 
           <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
             <h6 class="text-uppercase fw-bold">ĐƯỜNG DẪN</h6>
             <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
             <p><a href="#" class="text-white">Tài khoản của bạn</a></p>
-            <p><a href="#" class="text-white">Trở thành tiếp thị</a></p>
-            <p><a href="#" class="text-white">Chất lượng giao hàng</a></p>
+            <p><a href="#" class="text-white">Trở thành đồng đội</a></p>
+            <p><a href="#" class="text-white">Đánh giá và phản hồi</a></p>
             <p><a href="#" class="text-white">Hỗ trợ</a></p>
           </div>
 
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <h6 class="text-uppercase fw-bold">LIÊN LẠC</h6>
             <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-            <p><i class="fas fa-home mr-3"></i> Cần thơ, Việt Nam</p>
-            <p><i class="fas fa-envelope mr-3"></i> Trang@gmail.com</p>
-            <p><i class="fas fa-phone mr-3"></i> 090910JQK</p>
-            <p><i class="fas fa-print mr-3"></i> 090910JQK</p>
+            <p><i class="fas fa-home mr-3"></i> Hà Nội, Việt Nam</p>
+            <p><i class="fas fa-envelope mr-3"></i> Nhom1@gmail.com</p>
+            <p><i class="fas fa-phone mr-3"></i> 022334455</p>
+            <p><i class="fas fa-print mr-3"></i> 066778899</p>
           </div>
         </div>
       </div>
     </section>
 
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-      © 2024 Copyright: <a class="text-white" href="#">Trangcosmestics.com</a>
+      © 2025 Copyright: <a class="text-white" href="#">Nhom1.com</a>
     </div>
   </footer>
 </div>
